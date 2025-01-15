c71112cf7f581fbe1de0e18cb5130f9e30fcedc5
tbcarus <70680359+tbcarus@users.noreply.github.com>
2025-01-15 20:33:32 +0300

    Initial commit

---
+++ src/main/java/ru/tbcarus/jrfinal/JrFinalApplication.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.jrfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tbcarus/jrfinal/JrFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tbcarus.jrfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
